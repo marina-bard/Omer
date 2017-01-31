9ed90a7a09fb355a9f5777937b74237da8902d59
ArtuhovichVladislav <artvlad96@gmail.com>
2017-01-31 16:50:48 +0300

    Omer-4. Add login template

---
+++ src/Omer/UserBundle/Controller/SecurityController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marina
+ * Date: 22.7.16
+ * Time: 11.37
+ */
+
+namespace Omer\UserBundle\Controller;
+
+use FOS\UserBundle\Controller\SecurityController as BaseController;
+use Symfony\Component\HttpFoundation\Request;
+
+class SecurityController extends BaseController
+{
+    public function renderLogin(array $data)
+    {
+        $requestAttributes = $this->get('request_stack')->getCurrentRequest()->attributes;
+
+//        if ('admin_login' === $requestAttributes->get('_route')) {
+            $template = sprintf('OmerUserBundle:Security:login_admin.html.twig');
+//        } else {
+//            $template = sprintf('FOSUserBundle:Security:login.html.twig');
+//        }
+
+        return $this->get('templating')->renderResponse($template, $data);
+    }
+}

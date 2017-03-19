925dc3235df8dec888d5b8b2917e811ba4d79d26
Marina Bardiyan <marina.bard.96@gmail.com>
2017-03-19 22:57:25 +0300

    Omer-39. Add official user and login admin

--- src/Omer/UserBundle/Admin/OfficialUserAdmin.php
+++ src/Omer/UserBundle/Admin/OfficialUserAdmin.php
@@ -10,6 +10,7 @@ namespace Omer\UserBundle\Admin;
 
 use Omer\TeamBundle\Entity\BaseTeam;
 use Omer\UserBundle\Admin\PersonalDataAdmin;
+use Omer\UserBundle\Entity\OfficialUser;
 use Omer\UserBundle\Traits\CurrentUserTrait;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
@@ -32,28 +33,70 @@ class OfficialUserAdmin extends PersonalDataAdmin
 
     protected function configureFormFields(FormMapper $formMapper)
     {
-        if ($this->getCurrentUser()->hasRole('ROLE_SUPER_ADMIN')
-            || $this->getCurrentUser()->hasRole('ROLE_MAIN_ADMIN')
+        if (!$this->getSubject()->hasRole('ROLE_SUPER_ADMIN')
+            || !$this->getSubject()->hasRole('ROLE_MAIN_ADMIN')
         ) {
             $formMapper
-                ->add('username', TextType::class)
-                ->add('plainPassword', 'repeated', array(
-                    'type' => 'password',
-                    'options' => array('translation_domain' => 'FOSUserBundle'),
-                    'first_options' => array('label' => 'form.password'),
-                    'second_options' => array('label' => 'form.password_confirmation'),
-                    'invalid_message' => 'fos_user.password.mismatch'));
-            ;
-        }
-        else {
-            parent::configureFormFields($formMapper);
-
-            $formMapper
-                ->add('teamRole', TextType::class, [
-                    'label' => 'label.other_people.team_role'
+                ->add('roles', ChoiceType::class, [
+                    'choices' => OfficialUser::ROLES,
+                    'multiple' => true,
+                    'translation_domain' => 'OmerUserBundle',
+                    'attr' => ['readonly' => true]
+                ])
+                ->add('firstName', TextType::class, [
+                    'label' => 'label.personal_data.first_name',
+                    'translation_domain' => 'OmerUserBundle'
+                ])
+                ->add('surname', TextType::class, [
+                    'label' => 'label.personal_data.surname',
+                    'translation_domain' => 'OmerUserBundle'
+                ])
+                ->add('gender', ChoiceType::class, [
+                    'choices' => array_flip(OfficialUser::GENDER),
+                    'label' => 'label.user.gender',
+                    'translation_domain' => 'OmerUserBundle',
+                ])
+                ->add('T_shirtSize', TextType::class, [
+                    'label' => 'label.personal_data.t_shirt_size',
+                    'translation_domain' => 'OmerUserBundle'
+                ])
+                ->add('association', TextType::class, [
+                    'label' => 'label.user.association'
+                ])
+                ->add('citizenship', TextType::class, [
+                    'label' => 'label.personal_data.citizenship',
+                    'translation_domain' => 'OmerUserBundle'
                 ])
                 ->add('address', TextType::class, [
                     'label' => 'label.other_people.address'
+                ])
+                ->add('dateOfBirth', 'sonata_type_date_picker', [
+                    'format' => 'dd.MM.yyyy',
+                    'dp_view_mode' => 'years',
+                    'label' => 'label.personal_data.date_of_birth',
+                    'translation_domain' => 'OmerUserBundle',
+                    'attr' => ['readonly' => true]
+                ])
+                ->add('passportNumber', TextType::class, [
+                    'label' => 'label.personal_data.passport_number',
+                    'translation_domain' => 'OmerUserBundle'
+                ])
+                ->add('dateOfIssue', 'sonata_type_date_picker', [
+                    'format' => 'dd.MM.yyyy',
+                    'dp_view_mode' => 'years',
+                    'label' => 'label.personal_data.date_of_issue',
+                    'translation_domain' => 'OmerUserBundle',
+                    'attr' => ['readonly' => true]
+                ])
+                ->add('dateOfExpiry', 'sonata_type_date_picker', [
+                    'format' => 'dd.MM.yyyy',
+                    'dp_view_mode' => 'years',
+                    'label' => 'label.personal_data.date_of_expiry',
+                    'translation_domain' => 'OmerUserBundle',
+                    'attr' => ['readonly' => true]
+                ])
+                ->add('mobilePhone', TextType::class, [
+                    'label' => 'label.user.mobile_phone',
                 ])
                 ->add('email', EmailType::class, [
                     'label' => 'label.other_people.email'
@@ -73,18 +116,29 @@ class OfficialUserAdmin extends PersonalDataAdmin
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
         $datagridMapper
+            ->add('association', null,[
+                'label' => 'label.user.association'
+            ])
             ->add('surname', null, [
-                'label' => 'label.other_name.surname'
+                'label' => 'label.personal_data.surname'
             ])
         ;
     }
 
     protected function configureListFields(ListMapper $listMapper)
     {
+        dump($this->getBaseRouteName());
+        dump($this->getBaseRoutePattern());
         $listMapper
+            ->add('fullName', null,[
+                'label' => 'label.personal_data.full_name'
+            ])
             ->add('email', null, [
                 'label' => 'label.coach_user.email',
                 'translation_domain' => 'OmerUserBundle'
+            ])
+            ->add('association', null,[
+                'label' => 'label.user.association'
             ])
             ->add('_action', 'actions', [
                 'actions' => [
@@ -95,12 +149,7 @@ class OfficialUserAdmin extends PersonalDataAdmin
         ;
     }
 
-    protected function configureShowFields(ShowMapper $showMapper)
-    {
-    }
-
     // add query for role director and judge
-
 
     public function createQuery($context = 'list')
     {
@@ -109,10 +158,18 @@ class OfficialUserAdmin extends PersonalDataAdmin
          */
         $query = parent::createQuery($context);
 
-//        if ($this->getCurrentUser()->hasRole('ROLE_MAIN_ADMIN')) {
-//            $query
-//                ->andWhere($query->getRootAlias().'.roles LIKE \'%ROLE_SONATA_ADMIN%\'');
-//        }
+        if ($this->getCurrentUser()->hasRole('ROLE_MAIN_ADMIN')) {
+            $query
+                ->andWhere($query->getRootAlias().'.roles NOT LIKE :role')
+                ->setParameter('role', '%ROLE_SUPER_ADMIN%')
+            ;
+        }
+        elseif (!$this->getCurrentUser()->hasRole('ROLE_SUPER_ADMIN')) {
+            $query
+                ->andWhere($query->getRootAlias().'.email = :email')
+                ->setParameter('email', $this->getCurrentUser()->getEmail())
+            ;
+        }
 
         return $query;
     }
@@ -123,11 +180,5 @@ class OfficialUserAdmin extends PersonalDataAdmin
         ) {
             $object->setUsername($object->getEmail());
         }
-    }
-
-    public function preUpdate($object) {
-        $um = $this->getConfigurationPool()->getContainer()->get('fos_user.user_manager');
-        $um->updateCanonicalFields($object);
-        $um->updatePassword($object);
     }
 }

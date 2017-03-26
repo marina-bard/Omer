7a71529568524c444d43f66589a78e880fa72096
Marina Bardiyan <marina.bard.96@gmail.com>
2017-03-26 23:05:20 +0300

    Omer-43. Add only team saving

--- src/Omer/TeamBundle/Command/TransferTeamsCommand.php
+++ src/Omer/TeamBundle/Command/TransferTeamsCommand.php
@@ -9,11 +9,14 @@
 namespace Omer\TeamBundle\Command;
 
 
-use Symfony\Component\Console\Command\Command;
+use Omer\TeamBundle\Builder\TeamExcelBuilder;
+use Omer\TeamBundle\Entity\ForeignTeam;
+use Omer\TravelBundle\Entity\TravelInfo;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class TransferTeamsCommand extends Command
+class TransferTeamsCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
@@ -26,6 +29,51 @@ class TransferTeamsCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $files = glob($this->getContainer()->get('kernel')->getRootDir() . '/' . TeamExcelBuilder::TEAM_INFO_FILEPATH . '*.xls');
+
+        foreach ($files as $file) {
+            $object = \PHPExcel_IOFactory::load($file);
+            $sheet = $object->getActiveSheet();
+
+            $membershipNumber = $sheet->getCell('C3')->getValue();
+            $team = $em->getRepository('OmerTeamBundle:ForeignTeam')->findOneBy(['memberNumber' => $membershipNumber]);
+            if ($team) {
+                continue;
+            }
+
+            $team = new ForeignTeam();
+            $team->setMemberNumber($membershipNumber);
+            $team->setEnglishTeamName($sheet->getCell('C2')->getValue());
+            $team->setSchool($sheet->getCell('C4')->getValue());
+            $team->setCountry($sheet->getCell('C5')->getValue());
+            $team->setDictrict($sheet->getCell('C6')->getValue());
+            $team->setCity($sheet->getCell('C7')->getValue());
+            $team->setAddress($sheet->getCell('C8')->getValue());
+            $team->setProblem($sheet->getCell('C9')->getValue());
+            $team->setDivision($sheet->getCell('C10')->getValue());
+            $currency = $sheet->getCell('C11')->getValue();
+            $team->setPaymentCurrency(array_search($currency,ForeignTeam::PAYMENT_CURRENCY));
+            $team->setConcerns($sheet->getCell('C12')->getValue());
+
+            $row = 13;
+            /**
+             * @var TravelInfo $travelAttribute
+             */
+            foreach ($team->getTravelAttributes() as $travelAttribute) {
+                $date = \DateTime::createFromFormat('d-m-Y', $sheet->getCell('C'.(++$row))->getValue());
+                $travelAttribute->setDate($date);
+                $travelAttribute->setGoBy($sheet->getCell('C'.(++$row))->getValue());
+                $travelAttribute->setTransportNumber($sheet->getCell('C'.(++$row))->getValue());
+                $travelAttribute->setStationFrom($sheet->getCell('C'.(++$row))->getValue());
+                $travelAttribute->setStationTo($sheet->getCell('C'.(++$row))->getValue());
+                $travelAttribute->setTime($sheet->getCell('C'.(++$row))->getValue());
+            }
+
+            $em->persist($team);
+            break;
+        }
+        $em->flush();
         //get file
         //create command
         //parse file
